61a995719460a727a58ad836a5d78a86208947fc
ACHRAFJAOUAD <achrafjaouad237@gmail.com>
2024-04-07 17:21:45 +0000

    add some stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
 
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import AdminDashboard from "./components/admin/AdminDashboard";
-import StudentDashboard from "./components/student/StudentDashboard";
-import TeacherDashboard from "./components/teacher/TeacherDashboard";
+import NotFound from "./pages/NotFound.jsx";
+import Login from "./components/auth/Login.jsx";
+import Home from "./pages/Home.jsx";
+import AdminDashboard from "./components/AdminDashboard.jsx";
+import TeacherDashboard from "./components/TeacherDashboard.jsx";
+import StudentDashboard from "./components/StudentDashboard.jsx";
 
-function App() {
-  // const [currentUser, setCurrentUser] = useState(null);
-
+const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin/*" element={<AdminDashboard />} />
-        <Route path="/student/*" element={<StudentDashboard />} />
-        <Route path="/teacher/*" element={<TeacherDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
   );
-}
+};
 
 export default App;
