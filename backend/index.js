956b8b6e5434fbd690a540a7cc0cf2dcfa1a112f
ACHRAFJAOUAD <achrafjaouad237@gmail.com>
2024-04-21 22:46:51 +0000

    index for cors

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,13 @@ if (!fs.existsSync(profilePicturesDir)) {
 }
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use("/api/images", express.static("public/uploads/profile-pictures"));
 // Routes
