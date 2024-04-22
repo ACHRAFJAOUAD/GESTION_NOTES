ab8719c4929a379384bf0950129378a6e92d3bb8
ACHRAFJAOUAD <achrafjaouad237@gmail.com>
2024-04-22 14:55:19 +0000

    index

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,15 @@ if (!fs.existsSync(profilePicturesDir)) {
 }
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use("/api/images", express.static("public/uploads/profile-pictures"));
 // Routes
