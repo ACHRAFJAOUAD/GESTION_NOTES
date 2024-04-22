45dfcf4ea851ec8b76c41e03519da62655152cdb
ACHRAFJAOUAD <achrafjaouad237@gmail.com>
2024-04-22 14:51:51 +0000

    server again

--- frontend/src/server/server.js
+++ frontend/src/server/server.js
@@ -1,5 +1,3 @@
 const apiBaseUrl =
-  // "https://gestion-notes-backend.vercel.app"
-  // ||
-  "http://localhost:3001";
+  "https://gestion-notes-backend.vercel.app" || "http://localhost:3001";
 export default apiBaseUrl;
